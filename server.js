b92f5a3093216550cc1c842dd73cc791f6364457
koinworks <anggi.fergian@koinworks.com>
2022-08-12 15:15:26 +0700

    Add body-parser & cors

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 9000;
 const app = express();
 
-// Main route
+// Enable CORS
+app.use(cors())
+
+// Allow JSON request
+app.use(bodyParser.json())
+
+// Allow x-www-form-urlencoded request
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// Main route v1
 app.use('/api/v1', require('./router'))
 
+// Start server
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
