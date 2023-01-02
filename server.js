ea0aff59dc7181c124144d89364567ccdfac5243
koinworks <anggi.fergian@koinworks.com>
2023-01-02 15:11:14 +0700

    Refactor server.js

--- server.js
+++ server.js
@@ -2,14 +2,39 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const helmet = require('helmet');
+const morgan = require('morgan');
+const config = require('config');
+const debug = require('debug')('app:startup');
 
 const PORT = process.env.PORT || 9000;
 const app = express();
 
-app.use(helmet()); // Secure HTTP headers
-app.use(cors()); // Enable CORS
-app.use(bodyParser.json()); // Allow JSON request
-app.use(bodyParser.urlencoded({ extended: true })); // Allow x-www-form-urlencoded request
-app.use(express.static('public')); // Static files
+debug(`App name: ${config.get('name')}`);
+debug(`Mail service: ${config.get('mail.host')}`);
+debug(`Mail password: ${config.get('mail.password')}`);
+
+if (app.get('env') === 'development') {
+    app.use(morgan('common'));
+    debug('Morgan enabled...');
+}
+
+/**=== Secure HTTP headers ===*/
+app.use(helmet());
+
+/**=== Enable CORS ===*/
+app.use(cors());
+
+/**=== Allow JSON request ===*/
+app.use(bodyParser.json());
+
+/**=== Allow x-www-form-urlencoded request ===*/
+app.use(bodyParser.urlencoded({ extended: true }));
+
+/**=== Static files ===*/
+app.use(express.static('public'));
+
+/**=== Main Route ===*/
 app.use('/api/v1', require('./router'));
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+/**=== Listener ===*/
+app.listen(PORT, () => debug(`Listening on port ${PORT}`));
