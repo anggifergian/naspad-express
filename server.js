80b9ef7dfe1b93aafb21d725f7e1116d0b60a12a
Anggi Fergian <anggi.fergian@koinworks.com>
2023-01-19 08:17:34 +0700

    Connecting to mongodb

--- server.js
+++ server.js
@@ -4,10 +4,31 @@ const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
 const config = require('config');
+const mongoose = require('mongoose');
 const debug = require('debug')('app:startup');
+const socketDebug = require('debug')('app:socket');
+const mongodDebug = require('debug')('app:mongod');
+const { createServer } = require('http');
+const { Server } = require('socket.io');
 
 const PORT = process.env.PORT || 9000;
 const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+    cors: {
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST'],
+    }
+});
+
+io.on('connection', (socket) => {
+    socketDebug(`User connected ${socket.id}`);
+
+    socket.on('join_room', (data) => {
+        const { username } = data;
+        socketDebug('AF: username...', username);
+    });
+});
 
 debug(`App name: ${config.get('name')}`);
 debug(`Mail service: ${config.get('mail.host')}`);
@@ -17,6 +38,11 @@ if (app.get('env') === 'development') {
     app.use(morgan('common'));
     debug('Morgan enabled...');
 }
+
+mongoose.set('strictQuery', false);
+mongoose.connect('mongodb://localhost/playground')
+    .then(() => mongodDebug('Connected to MongoDB...'))
+    .catch(err => mongodDebug('Could not connect to MongoDB', err));
 
 /**=== Secure HTTP headers ===*/
 app.use(helmet());
@@ -37,4 +63,4 @@ app.use(express.static('public'));
 app.use('/api/v1', require('./router'));
 
 /**=== Listener ===*/
-app.listen(PORT, () => debug(`Listening on port ${PORT}`));
+httpServer.listen(PORT, () => debug(`Listening on port ${PORT}`));
