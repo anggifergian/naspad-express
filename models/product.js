97643dbc76f825976ba30b2a32d98b6d10f67f83
Anggi Fergian <anggi.fergian@koinworks.com>
2023-01-23 01:44:17 +0700

    Add categories property in product model

--- models/product.js
+++ models/product.js
@@ -3,13 +3,17 @@
 const mongoose = require('mongoose');
 const moment = require('moment');
 const Joi = require('joi');
+Joi.objectId = require('joi-objectid')(Joi);
+
+const min_title_length = 3;
+const max_title_length = 255;
 
 const Product = mongoose.model('Product', new mongoose.Schema({
     title: {
         type: String,
         required: true,
-        minlength: 5,
-        maxlength: 255,
+        minlength: min_title_length,
+        maxlength: max_title_length,
     },
     price: {
         type: Number,
@@ -20,6 +24,19 @@ const Product = mongoose.model('Product', new mongoose.Schema({
         type: String,
         default: '',
     },
+    categories: {
+        type: [{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Category',
+            required: true,
+        }],
+        validate: {
+            validator: function (v) {
+                return v && v.length > 0;
+            },
+            message: 'A product should have at least one category.'
+        },
+    },
     createdAt: {
         type: Date,
         required: true,
@@ -29,15 +46,43 @@ const Product = mongoose.model('Product', new mongoose.Schema({
 
 function validateProduct(data) {
     const shcema = Joi.object({
-        title: Joi.string().min(5).max(255).required(),
+        title: Joi
+            .string()
+            .min(min_title_length)
+            .max(max_title_length)
+            .required(),
         price: Joi.number().max(99_999_999),
         imageUrl: Joi.string(),
+        categories: Joi
+            .array()
+            .items(Joi.object({ id: Joi.objectId() }))
+            .required(),
     });
 
     return shcema.validate(data);
 }
 
+function mapCategories(items) {
+    return items.map(item => item['id']);
+}
+
+function compareCategories(fromReq, fromDb) {
+    const invalidIDs = [];
+
+    fromReq.filter(objectID => {
+        const isExist = fromDb.find(category => category._id.equals(objectID));
+
+        if (!isExist) {
+            invalidIDs.push(objectID);
+        }
+    });
+
+    return invalidIDs;
+}
+
 module.exports = {
     Product,
     validateProduct,
+    mapCategories,
+    compareCategories,
 }
