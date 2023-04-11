09a3ebe04ec2b0e446d518da778e19ec82bca58a
anggifergian <anggyfergian56@gmail.com>
2023-04-11 12:28:02 +0700

    Add checkValidUUID func

--- utils/mongoose.js
+++ utils/mongoose.js
@@ -1,5 +1,10 @@
 const mongoose = require('mongoose');
+const { validate: uuidValidate } = require('uuid')
 
 module.exports.isValidID = function(id) {
     return mongoose.Types.ObjectId.isValid(id);
+}
+
+module.exports.checkValidUUID = function(uuid) {
+    return uuidValidate(uuid)
 }
