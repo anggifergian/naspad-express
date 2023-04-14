7ac7dcf30eb6b8e636a44256a9ab146f65c7d3eb
anggifergian <anggyfergian56@gmail.com>
2023-04-14 09:59:24 +0700

    Encrypt password using bcrypt and set token on header

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const _ = require('lodash');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 const { sendResponse, modify } = require('../utils/response');
 const { User, validateUser } = require('../models/user');
@@ -18,15 +22,19 @@ router.post('/', async (req, res) => {
             return sendResponse(res, { statusCode: 400, message: errMessage });
         }
 
-        user = new User({
-            name: req.body.name,
-            email: req.body.email,
-            password: req.body.password,
-        });
+        user = new User(_.pick(req.body, ['name', 'email','password']));
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(user.password, salt);
 
         await user.save();
 
-        sendResponse(res, { message: 'Data created', data: user });
+        const token = jwt.sign({ _id: user._id }, config.get('jwtPrivateKey'));
+
+        res.header('x-auth-token', token).send({
+            status: true,
+            message: 'Data created',
+            data: _.pick(user, ['_id', 'name', 'email'])
+        });
     } catch (error) {
         sendResponse(res, { statusCode: 500, message: error['message'] });
     }
