46f886bc7da0489633a017e7facec35e826425a0
anggifergian <anggyfergian56@gmail.com>
2023-04-15 15:01:04 +0700

    Apply logger on error and morgan middleware

--- middleware/error.js
+++ middleware/error.js
@@ -1,5 +1,8 @@
 const { sendResponse } = require('../utils/response');
+const logger = require('../utils/logger');
 
 module.exports = function(err, req, res, next) {
+    logger.error(err['message'], err);
+
     sendResponse(res, { statusCode: 500, message: err['message'] });
 }
