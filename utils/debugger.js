0324c7e991426f5b70e4db744f4ab7e400f47a81
anggifergian <anggyfergian56@gmail.com>
2023-05-20 14:23:44 +0700

    Create unit testing for user models

--- utils/debugger.js
+++ utils/debugger.js
@@ -1,9 +1,7 @@
 const startupDebug = require('debug')('app:startup');
 const socketDebug = require('debug')('app:socket');
-const mongodDebug = require('debug')('app:mongod');
 
 module.exports = {
     startupDebug,
     socketDebug,
-    mongodDebug,
 }
