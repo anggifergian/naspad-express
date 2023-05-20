44217e803ff7434b1f2ba8215dbf573bb98572e1
anggifergian <anggyfergian56@gmail.com>
2023-05-20 18:49:58 +0700

    [2] Add db config - default and test

--- startup/db.js
+++ startup/db.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
+const config = require('config');
 const logger = require('../utils/logger');
 
 module.exports = function () {
+    const db_string = config.get('db');
     mongoose.set('strictQuery', false);
-    mongoose.connect('mongodb://localhost/playground')
-        .then(() => logger.info('Connected to MongoDB...'));
+    mongoose.connect(db_string)
+        .then(() => logger.info(`Connected to ${db_string}...`));
 }
