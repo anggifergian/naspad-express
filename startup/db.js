c071807ed99ac38ea74bb658fb7c00005cb30a58
Anggi Fergian <anggyfergian56@gmail.com>
2024-07-22 11:53:32 +0700

    modify startup - db

--- startup/db.js
+++ startup/db.js
@@ -1,8 +1,21 @@
 const mongoose = require('mongoose');
 const logger = require('../utils/logger');
+const { getEnvVar } = require('../utils/config');
 
-module.exports = function () {
+module.exports = async function (app) {
     mongoose.set('strictQuery', false);
-    mongoose.connect('mongodb://localhost/playground')
-        .then(() => logger.info('Connected to MongoDB...'));
+
+    const mongoUri = getEnvVar('MONGO_URI', 'mongodb://localhost:27017/playground');
+
+    mongoose.connect(mongoUri)
+        .then(() => logger.info(`Connected to MongoDB at ${mongoUri}...`))
+        .catch((err) => {
+            logger.error(`Could not connect to MongoDB at ${mongoUri}.`, err);
+
+            if (app) {
+                app.use((req, res, next) => {
+                    res.status(500).send('Internal Server Error: Could not connect to the database.');
+                });
+            }
+        })
 }
